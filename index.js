8ad81c734295e83183fdda3a012f79e32f555ec0
deveshanim3 <deveshsingh6986@gmail.com>
2024-12-10 10:50:45 +0530

    made score,hit,timer functional and improved ui

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var score=0;
+var timer=60;
+var newHit;
 function makeBubble(){
     var clutter=``;
     for(var i=1;i<=102;i++){
@@ -7,26 +10,47 @@ function makeBubble(){
     document.querySelector('#btm').innerHTML=clutter;
 }
 
-var timer=60;
 function clock(){
     setInterval(()=>{
         timer--;
         if(timer<0)return ;
         document.querySelector('#timer').textContent=timer;
+        if(timer==0){
+            window.location.href='score.html';
+        }
     },1000)
 }
 
 function getNewHit(){
-    var newHit=Math.floor(Math.random()*(10-1)+1);
+    newHit=Math.floor(Math.random()*(10-1)+1);
     document.querySelector('#hit').textContent=newHit;
 
 }
 
-var score=0;
 function increaseScore(){
     score+=10;
     document.querySelector('#score').textContent=score;
 }
+
+function decreaseScore(){
+    score-=10;
+    document.querySelector('#score').textContent=score;
+}
+
+document.querySelector('#btm').addEventListener('click',(detail)=>{
+    var clickedNum=Number(detail.target.textContent);
+    console.log(clickedNum);
+    if (newHit===clickedNum) {
+        increaseScore()
+        getNewHit()
+        makeBubble()
+    }
+    else if(newHit!==clickedNum && clickedNum%1==0){
+        decreaseScore()
+        getNewHit()
+        makeBubble()
+    }  
+})
 
 
 clock()
